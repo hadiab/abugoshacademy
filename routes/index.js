18cb31b1cbd0839cb3f0799642357ae8cfaeb415
hadiab <hipro4d@hotmail.com>
2017-04-20 16:39:25 +0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { page: 'home' });
+});
+
+router.get('/team', function(req, res, next) {
+  res.render('team', { page: 'team' });
+});
+
+module.exports = router;
